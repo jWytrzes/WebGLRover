9f57541312b73bd173d294daa9f5716bfba681d3
KonradWrbl <160815@stud.prz.edu.pl>
2020-05-17 14:53:45 +0200

    polimorphism

--- classes/Cuboid.js
+++ classes/Cuboid.js
@@ -1,14 +1,18 @@
-export default class Cuboid {
+import Structure from './Structure.js'
+
+export default class Cuboid extends Structure {
     constructor(pos, w, h, d) {
+        super();
         this.x = pos.x;
         this.y = pos.y;
         this.z = pos.z;
         this.width = w;
         this.height = h;
         this.depth = d;
+        this.create();
     }
 
-    render() {
+    create() {
         const cubeGeometry = new THREE.BoxGeometry(this.w, this.h, this.d);
         const cubeMaterial = new THREE.MeshPhongMaterial({
             color: new THREE.Color('hsl(40, 100%, 60%)'),
@@ -18,7 +22,7 @@ export default class Cuboid {
         cuboid.position.x = this.x
         cuboid.position.y = this.y
         cuboid.position.z = this.z
-        return cuboid;
+        this.structure = cuboid
     }
 }
 
