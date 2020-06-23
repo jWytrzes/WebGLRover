83ea0081030d05d9b39e052e7b484a8314366472
Joanna Wytrzes <joannawytrzes@gmail.com>
2020-06-23 19:18:04 +0200

    Imporve rover y position

--- classes/View.js
+++ classes/View.js
@@ -9,7 +9,7 @@ var clock = new THREE.Clock();
 var rover;
 let rovPos = {
 	x: -20,
-	y: 37.4,
+	y: 36,
 	z: 20,
 };
 let rovRot = 0;
@@ -54,7 +54,7 @@ export default class View {
 		this.structure = terrain;
 
 		//łazik
-		const roverPosition = new Position(-20, 37.4, 20);
+		const roverPosition = new Position(-20, 36, 20);
 		rover = new Rover(roverPosition);
 		this.scene.add(rover.render());
 
@@ -79,24 +79,30 @@ export default class View {
 		var rotateAngle = (Math.PI / 2) * delta; // pi/2 radians (90 degrees) per second
 
 		if (keyboard.pressed('W')) {
-			rovPos.x += moveDistance * Math.cos(rovRot)
+			rovPos.x += moveDistance * Math.cos(rovRot);
 			rovPos.z -= moveDistance * Math.sin(rovRot);
-			rover.updatePos(rovPos.x -(moveDistance * Math.cos(0)), rovPos.y, rovPos.z + (moveDistance * Math.sin(rovRot)));
+			rover.updatePos(
+				rovPos.x - moveDistance * Math.cos(0),
+				rovPos.y,
+				rovPos.z + moveDistance * Math.sin(rovRot),
+			);
 		}
 		if (keyboard.pressed('S')) {
 			rovPos.x -= moveDistance * Math.cos(rovRot);
 			rovPos.z += moveDistance * Math.sin(rovRot);
-			rover.updatePos(rovPos.x + (moveDistance * Math.cos(rovRot)), rovPos.y , rovPos.z - (moveDistance * Math.sin(rovRot)));
-			console.log(Math.round(Math.cos(Math.PI*3/2)))
+			rover.updatePos(
+				rovPos.x + moveDistance * Math.cos(rovRot),
+				rovPos.y,
+				rovPos.z - moveDistance * Math.sin(rovRot),
+			);
 		}
 		if (keyboard.pressed('A')) {
-			rovRot += rotateAngle
-			rover.updateRot(rovRot - rotateAngle)
-			console.log(rovRot)
+			rovRot += rotateAngle;
+			rover.updateRot(rovRot - rotateAngle);
 		}
 		if (keyboard.pressed('D')) {
-			rovRot -= rotateAngle
-			rover.updateRot(rovRot + rotateAngle)
+			rovRot -= rotateAngle;
+			rover.updateRot(rovRot + rotateAngle);
 		}
 
 		this.controls.update();
